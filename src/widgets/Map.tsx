df4a1bda064b6a7590e45435c9a34e4cf2d247ae
odoe <rrubalcava@esri.com>
2019-03-20 08:20:41 -0700

    fix formatting

--- src/widgets/Map.tsx
+++ src/widgets/Map.tsx
@@ -1,27 +1,27 @@
-import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
-import MetaBase from '@dojo/framework/widget-core/meta/Base';
-import { tsx } from '@dojo/framework/widget-core/tsx';
+import WidgetBase from "@dojo/framework/widget-core/WidgetBase";
+import MetaBase from "@dojo/framework/widget-core/meta/Base";
+import { tsx } from "@dojo/framework/widget-core/tsx";
 
-import * as css from './styles/Map.m.css';
+import * as css from "./styles/Map.m.css";
 
 class HtmlMeta extends MetaBase {
-	get(key: string): Element {
-			const node = this.getNode(key);
-			return node as Element;
-	}
+  get(key: string): Element {
+    const node = this.getNode(key);
+    return node as Element;
+  }
 }
 
 export interface MapProperties {
-	webmapid: string;
+  webmapid: string;
   initializeMap: (container: HTMLElement) => void;
 }
 
 export default class Map extends WidgetBase<MapProperties> {
-	onAttach() {
-		const element = this.meta(HtmlMeta).get('elemRef') as HTMLElement;
-		this.properties.initializeMap(element);
-}
-	protected render() {
-		return <div classes={css.root} key='elemRef'></div>;
-	}
+  onAttach() {
+    const element = this.meta(HtmlMeta).get("elemRef") as HTMLElement;
+    this.properties.initializeMap(element);
+  }
+  protected render() {
+    return <div classes={css.root} key="elemRef" />;
+  }
 }
